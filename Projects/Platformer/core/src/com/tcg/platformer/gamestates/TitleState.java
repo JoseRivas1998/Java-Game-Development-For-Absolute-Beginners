7629d39bc2da157fefef35c89073fa3e4e0a2206
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-05-06 09:20:42 -0700

    Added something to title

--- Projects/Platformer/core/src/com/tcg/platformer/gamestates/TitleState.java
+++ Projects/Platformer/core/src/com/tcg/platformer/gamestates/TitleState.java
@@ -3,10 +3,20 @@ package com.tcg.platformer.gamestates;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.tcg.platformer.entities.TilingBackground;
+import com.tcg.platformer.managers.ContentManager;
 import com.tcg.platformer.managers.GameStateManager;
 import com.tcg.platformer.managers.input.MyInput;
 
+import static com.tcg.platformer.GameData.WORLD_HEIGHT;
+import static com.tcg.platformer.GameData.WORLD_WIDTH;
+
 public class TitleState extends AbstractGameState {
+
+    private Viewport viewport;
+    private TilingBackground background;
 
     public TitleState(GameStateManager gsm) {
         super(gsm);
@@ -14,6 +24,8 @@ public class TitleState extends AbstractGameState {
 
     @Override
     protected void init() {
+        viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT);
+        background = new TilingBackground(ContentManager.Image.LEVEL_BG, WORLD_WIDTH, WORLD_HEIGHT);
 
     }
 
@@ -29,17 +41,20 @@ public class TitleState extends AbstractGameState {
 
     @Override
     public void update(float dt) {
-
+        viewport.apply(true);
     }
 
     @Override
     public void draw(float dt, SpriteBatch sb, ShapeRenderer sr) {
-
+        sb.begin();
+        sb.setProjectionMatrix(viewport.getCamera().combined);
+        background.draw(sb);
+        sb.end();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height, true);
     }
 
     @Override
