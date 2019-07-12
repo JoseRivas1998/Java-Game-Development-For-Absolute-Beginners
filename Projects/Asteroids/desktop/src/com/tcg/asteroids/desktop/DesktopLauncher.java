6a7ea44741dd95353c521dd67ce2c34a675c6f41
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-07-12 14:01:06 -0700

    Generated Asteroids Project

---
+++ Projects/Asteroids/desktop/src/com/tcg/asteroids/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tcg.asteroids.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tcg.asteroids.Asteroids;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Asteroids(), config);
+	}
+}
